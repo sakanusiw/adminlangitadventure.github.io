bd046eb877bc2113fdc4a4501fdbf8320a153cfb
sakanusiw <saka.interisti@gmail.com>
2024-11-29 15:39:10 +0700

    hid the sensitive tokens

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLviB4xzfZ-eFkIm7X0_39cNmE17mYfLs",
-  authDomain: "camp-rental-84848.firebaseapp.com",
-  projectId: "camp-rental-84848",
-  storageBucket: "camp-rental-84848.appspot.com",
-  messagingSenderId: "926167421876",
-  appId: "1:926167421876:web:bb15efc69cec6c4a3198e1",
-  measurementId: "G-MHETFDYVB9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
